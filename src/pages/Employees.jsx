2e693c54124290e2c622ae5f4c21374b5cae616e
Samuel Guo <sguo114@gmail.com>
2022-10-10 14:50:17 -0400

    add orders, employees, customer pages and calendar, kanban

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Search, Inject, Toolbar} from '@syncfusion/ej2-react-grids'
 
-const Employees = () => {
+import { employeesData, employeesGrid } from '../data/dummy'
+import { Header } from '../components'
+
+const Orders = () => {
   return (
-    <div>Employees</div>
+    <div className='m-14 md:m-10 p-2 md:p-6 bg-white rounded-3xl'>
+      <Header category="Page" title="Employees"/>
+      <GridComponent
+        id="gridcomp"
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        toolbar={['Search']}
+        width="auto">
+        <ColumnsDirective>
+        {employeesGrid.map((item,index) => (
+          <ColumnDirective key={index} {...item}/>
+        ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar]}/>
+      </GridComponent>
+    </div>
   )
 }
 
-export default Employees
+export default Orders
